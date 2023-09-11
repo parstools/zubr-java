5780b78af79ccc324c0be96820485f2d236e0b50
Andrzej Borucki <borucki.andrzej@gmail.com>
2023-09-11 16:39:54 +0200

    start test first1/follow1 for grammar from files

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,62 @@
+import grammar.Grammar;
+import set.SetContainer;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.System.out;
+
 public class Main {
+    static void testFF1(List<String> lines) {
+        int n = 0;
+        int counter = 0;
+        while (n<lines.size()) {
+            List<String> gramLines = new ArrayList<>();
+            n = readGramLines(lines, n, gramLines);
+            Grammar grammar = new Grammar(gramLines);
+            SetContainer sc = new SetContainer(grammar);
+            sc.firstSetsByGeneration(1);
+            counter++;
+            n++;
+            n = readExpect1Lines(lines, n);
+            n++;
+        }
+        out.println(counter);
+    }
+
+    private static int readExpect1Lines(List<String> lines, int n) {
+        while(n<lines.size()) {
+            String line = lines.get(n).trim();
+            if (line.isEmpty()) break;
+            n++;
+        }
+        return n;
+    }
+
+    private static int readGramLines(List<String> lines, int n, List<String> gramLines) {
+        while(n<lines.size()) {
+            String line = lines.get(n).trim();
+            if (line.isEmpty() || line.equals("---")) break;
+            n++;
+            gramLines.add(line);
+        }
+        return n;
+    }
+
     public static void main(String[] args) {
+        Path path = Paths.get("res/test1.dat");
+        try {
+            List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+            lines.stream().forEach(out::println);
+            testFF1(lines);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/set/SetContainer.java
@@ -0,0 +1,13 @@
+package set;
+
+import grammar.Grammar;
+
+public class SetContainer {
+    Grammar grammar;
+    public SetContainer(Grammar grammar) {
+        this.grammar = grammar;
+    }
+
+    public void firstSetsByGeneration(int k) {
+    }
+}
