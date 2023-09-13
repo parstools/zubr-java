b478fee7d504ae1ffdd9bf0c87f2a3a6a9c9cb3a
Andrzej Borucki <borucki.andrzej@gmail.com>
2023-09-13 18:09:56 +0200

    grammar exception classes

---
+++ src/util/GrammarException.java
@@ -0,0 +1,7 @@
+package util;
+
+public class GrammarException extends RuntimeException {
+    public GrammarException(String errorMessage, Throwable err) {
+        super(errorMessage, err);
+    }
+}
---
+++ src/util/NoMinLenGrammarException.java
@@ -0,0 +1,7 @@
+package util;
+
+public class NoMinLenGrammarException extends GrammarException {
+    public NoMinLenGrammarException(String errorMessage, Throwable err) {
+        super(errorMessage, err);
+    }
+}
