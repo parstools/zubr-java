b3a5447fe5cdc8d86aa32e5a9f6f003afb13c8f6
Andrzej Borucki <borucki.andrzej@gmail.com>
2023-09-13 14:30:45 +0200

    comparing with test, 1 failed

--- src/Main.java
+++ src/Main.java
@@ -15,28 +15,42 @@ public class Main {
     static void testFF1(List<String> lines) {
         int n = 0;
         int counter = 0;
+        int counterFailed = 0;
         long start = System.nanoTime();
         while (n<lines.size()) {
             List<String> gramLines = new ArrayList<>();
             n = readGramLines(lines, n, gramLines);
             Grammar grammar = new Grammar(gramLines);
-            SetContainer sc = new SetContainer(grammar);
+            SetContainer sc1 = new SetContainer(grammar);
             out.println(counter);
-            sc.computeSetsByGeneration(1);
+            sc1.computeSetsByGeneration(1, 12);
             counter++;
             n++;
-            n = readExpect1Lines(lines, n);
+            List<String> expectLines = new ArrayList<>();
+            n = readExpect1Lines(lines, n, expectLines);
+            SetContainer sc2 = new SetContainer(grammar);
+            sc2.readTest1(expectLines);
+            if (sc1.hashCode()==sc2.hashCode())
+                out.println("OK");
+            else {
+                counterFailed++;
+                out.println(sc1);
+                out.println(sc2);
+                out.println("differ..");
+            }
             n++;
         }
         out.println(counter);
+        out.println("failed "+counterFailed);
         long duration = System.nanoTime()-start;
         out.println("duration="+duration/1e9);
     }
 
-    private static int readExpect1Lines(List<String> lines, int n) {
+    private static int readExpect1Lines(List<String> lines, int n, List<String> expectLines) {
         while(n<lines.size()) {
             String line = lines.get(n).trim();
             if (line.isEmpty()) break;
+            expectLines.add(line);
             n++;
         }
         return n;
@@ -47,7 +61,6 @@ public class Main {
             String line = lines.get(n).trim();
             if (line.isEmpty() || line.equals("---")) break;
             n++;
-            out.println(line);
             gramLines.add(line);
         }
         return n;
--- src/set/SetContainer.java
+++ src/set/SetContainer.java
@@ -2,6 +2,7 @@ package set;
 
 import generator.Generator;
 import grammar.Grammar;
+import util.Hash;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,20 +14,74 @@ public class SetContainer {
 
     List<TokenSet> firstSets = new ArrayList<>();
     List<TokenSet> followSets = new ArrayList<>();
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (TokenSet ts: firstSets)
+            sb.append(ts.toString());
+        sb.append(" | ");
+        for (TokenSet ts: followSets)
+            sb.append(ts.toString());
+        sb.append("]");
+        return sb.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        Hash h = new Hash();
+        for (TokenSet ts: firstSets)
+            h.add(ts.hashCode());
+        for (TokenSet ts: followSets)
+            h.add(ts.hashCode());
+        return h.hash();
+    }
+
     public SetContainer(Grammar grammar) {
         this.grammar = grammar;
     }
 
-    public void computeSetsByGeneration(int k) {
+    public void computeSetsByGeneration(int k, int maxLen) {
         firstSets.clear();
         followSets.clear();
-        Generator generator = new Generator(grammar, 7);
+        Generator generator = new Generator(grammar, maxLen);
         for (int i=0; i<grammar.nonterminals.size(); i++) {
             TokenSet firstSet = generator.collectFirstAllGenerated(i, k);
             firstSets.add(firstSet);
-            out.println(firstSet.hashCode());
             TokenSet followSet = generator.collectFollowAllGenerated(i, k);
             followSets.add(followSet);
+        }
+    }
+
+    public void readTest1(List<String> lines) {
+        firstSets.clear();
+        followSets.clear();
+        for (int i=1; i<lines.size(); i++) {
+            String line = lines.get(i);
+            String[] parts = line.split("\t");
+            assert(parts.length==4);
+            int nt = grammar.findSymbol(parts[0]).index;
+            out.println(nt);
+            boolean nullable = parts[1].equals("✔");
+            TokenSet first = new TokenSet(grammar,1);
+            if (nullable) {
+                Sequence seq = new Sequence(grammar, "");
+                first.addSeq(seq);
+            }
+
+            String[] parts2 = parts[2].split(", ");
+            for (String part: parts2) {
+                Sequence seq = new Sequence(grammar, part);
+                first.addSeq(seq);
+            }
+            TokenSet follow = new TokenSet(grammar,1);
+            String[] parts3 = parts[3].split(", ");
+            for (String part: parts3) {
+                Sequence seq = new Sequence(grammar, part);
+                follow.addSeq(seq);
+            }
+            firstSets.add(first);
+            followSets.add(follow);
         }
     }
 }
