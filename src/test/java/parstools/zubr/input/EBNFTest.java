0c373e4e956af2debb1b58b8e955365a81ffccd3
Andrzej Borucki <borucki.andrzej@gmail.com>
2024-11-11 17:30:27 +0100

    continuation EBNF conversion - tests failed

--- src/test/java/parstools/zubr/input/EBNFTest.java
+++ src/test/java/parstools/zubr/input/EBNFTest.java
@@ -2,9 +2,20 @@ package parstools.zubr.input;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class EBNFTest {
+
+    String[] fromSecondElement(String[] originalArray) {
+        String[] newArray = new String[originalArray.length - 1];
+        for (int i = 1; i < originalArray.length; i++) {
+            newArray[i - 1] = originalArray[i];
+        }
+        return newArray;
+    }
+
     @Test
-    void convertToLeft() {
+    void convertToLR() {
         //loops changes to left recursion, suitable to LR parsers
         String[][] testCases = {
                 {"A->a?", "A->a", "A->"},
@@ -18,32 +29,60 @@ public class EBNFTest {
             EBNF ebnf=new EBNF(false);
             ebnf.add(input);
             ebnf.convert();
+            String[] expected = fromSecondElement(testCase);
             String[] actual = ebnf.toLines();
-            for (int j = 1; j < testCase.length; j++) {
-                System.out.println("Element " + j + ": " + testCase[j]);
-            }
+            assertEquals(expected, actual);
         }
     }
 
     @Test
-    void convertToRight() {
+    void convertToLL() {
         //loops changes to right recursion, suitable to LL parsers
         String[][] testCases = {
                 {"A->a?", "A->a", "A->"},
-                {"A->a+", "A->Aa", "A->a"},
-                {"A->a*", "A->Aa", "A->"},
-                {"A->ab+c", "A->aBc","B->Bb","B->b"},
-                {"A->a(Bc)*d?", "A->aCD","C->CBc","C->","D->d","D->"}
+                {"A->a+", "A->aA", "A->a"},
+                {"A->a*", "A->aA", "A->"},
+                {"A->ab+c", "A->aBc","B->bB","B->b"},
+                {"A->a(Bc)*d?", "A->aCD","C->BcC","C->","D->d","D->"}
         };
         for (String[] testCase : testCases) {
             String input = testCase[0];
             EBNF ebnf=new EBNF(true);
             ebnf.add(input);
             ebnf.convert();
+            String[] expected = fromSecondElement(testCase);
             String[] actual = ebnf.toLines();
-            for (int j = 1; j < testCase.length; j++) {
-                System.out.println("Element " + j + ": " + testCase[j]);
-            }
+            assertEquals(expected, actual);
+        }
+    }
+
+    @Test
+    void twoLines() {
+        String[] input = {
+                "A->Ba*",
+                "A->b+",
+        };
+        String[] expected = {
+                "A->BC",
+                "B->bB",
+                "B->b",
+                "C->aC",
+                "C->",
+        };
+        String[][] testCases = {
+                {"A->a?", "A->a", "A->"},
+                {"A->a+", "A->aA", "A->a"},
+                {"A->a*", "A->aA", "A->"},
+                {"A->ab+c", "A->aBc","B->bB","B->b"},
+                {"A->a(Bc)*d?", "A->aCD","C->BcC","C->","D->d","D->"}
+        };
+        for (String[] testCase : testCases) {
+            EBNF ebnf=new EBNF(true);
+            for (String line: input)
+                ebnf.add(line);
+            ebnf.convert();
+            String[] actual = ebnf.toLines();
+            assertEquals(expected, actual);
         }
     }
 }
